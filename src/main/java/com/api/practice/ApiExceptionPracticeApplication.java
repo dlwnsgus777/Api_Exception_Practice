f5cffc5ef872923b341d223d36d4abbd0f033f8b
Lee, JunHyun <dlwnsgus777@naver.com>
2020-04-16 15:40:25 +0900

    feat: json 리턴, 예외 처리 연습 프로젝트 생성

---
+++ src/main/java/com/api/practice/ApiExceptionPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.api.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExceptionPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExceptionPracticeApplication.class, args);
+	}
+
+}
